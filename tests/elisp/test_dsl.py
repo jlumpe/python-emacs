9d0931b1aa4832f1fa64b2689cbe5a2331726859
Jared Lumpe <mjlumpe@gmail.com>
2022-01-13 18:00:56 -0700

    WIP DSL tests

---
+++ tests/elisp/test_dsl.py
@@ -0,0 +1,18 @@
+import emacs.elisp as el
+from emacs.elisp import E
+
+
+def test_getattr():
+	assert E.foo == el.Symbol('foo')
+	assert E.foo_bar == el.Symbol('foo-bar')
+	assert E.__call__ == object.__getattribute__(E, '__call__')
+	assert E.Q is el.quote
+
+
+def test_getitem():
+	assert E['foo'] == el.Symbol('foo')
+	assert E['foo_bar'] == el.Symbol('foo_bar')
+
+
+def test_call():
+	pass  # TODO
